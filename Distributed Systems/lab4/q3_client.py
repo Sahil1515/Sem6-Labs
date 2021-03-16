bbd3d9b96ec882d1ebc8531db58cca2ab1f95075
sahil <sahilsainisalaria@gmail.com>
2021-03-16 17:45:35 +0530

    DS LAB

---
+++ Distributed Systems/lab4/q3_client.py
@@ -0,0 +1,21 @@
+import socket
+HOST = '172.16.58.54'
+PORT = 2003
+s = socket.socket()
+name = input(str("\nEnter your name: "))
+s.connect((HOST, PORT))
+s.send(name.encode())
+s_name = s.recv(1024)
+s_name = s_name.decode()
+print(s_name, "has joined the chat room\nEnter E to exit chat room\n")
+while True:
+    message = s.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
+    message = input(str("Me : "))
+    if message == "E":
+        message = "Left chat room!"
+        s.send(message.encode())
+        print("\n")
+        break
+    s.send(message.encode())
